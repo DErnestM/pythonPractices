b1b967f558eb4a6426bbac2a3aa734e71febe887
MendozaDiego <diego@mendozadiego.com>
2020-05-13 11:51:50 -0500

    Clase 3

---
+++ instancias.py
@@ -0,0 +1,16 @@
+print ("Hello world!")
+
+class Persona:
+
+    def __init__(self, nombre, edad):
+        self.nombre = nombre
+        self.edad = edad
+    
+    def saluda(self, otra_persona):
+        print(f'Hola {otra_persona.nombre}, me llamo {self.nombre}')
+
+
+david = Persona('David', 35)
+erika = Persona('Erika', 32)
+
+david.saluda(erika)
